28c429fc36e22c82ea12c69c16e91494a755abe8
weilai5188 <4242887@qq.com>
2026-05-01 12:03:13 +0800

    启用扩展板音量和打断按键GPIO

--- main/boards/bread-compact-wifi-lcd/config.h
+++ main/boards/bread-compact-wifi-lcd/config.h
@@ -29,10 +29,16 @@
 
 
 #define BUILTIN_LED_GPIO        GPIO_NUM_48
+
+// BoChain expansion board v1
+// GPIO38 = Volume +
+// GPIO39 = Volume -
+// GPIO0  = Interrupt / BOOT
+// GPIO2  = Interrupt / Touch button
 #define BOOT_BUTTON_GPIO        GPIO_NUM_0
-#define TOUCH_BUTTON_GPIO       GPIO_NUM_NC
-#define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_NC
-#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_NC
+#define TOUCH_BUTTON_GPIO       GPIO_NUM_2
+#define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_38
+#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_39
 
 
 #define DISPLAY_BACKLIGHT_PIN GPIO_NUM_42
